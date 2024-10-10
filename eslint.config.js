f7595626ba6ddce017e34c93ba286a921d424ca9
Cassiano Laudemiro Expedito <cassianolaudemiro1@gmail.com>
2024-10-10 00:30:50 -0300

    Update eslint.config.js
    renovação

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
+// eslint.config.js
+import react from 'eslint-plugin-react'
+
 export default tseslint.config({
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
+  // Set the react version
+  settings: { react: { version: '18.3' } },
+  plugins: {
+    // Add the react plugin
+    react,
+  },
+  rules: {
+    // other rules...
+    // Enable its recommended rules
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
   },
 })
